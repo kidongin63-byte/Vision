49eafc59a5fe16611b18ed8fbebb6b64852f4f5d
kidongin63-byte <kidongin63@gmail.com>
2026-03-18 17:09:36 +0900

    Update app_fixed.py

--- app_fixed.py
+++ app_fixed.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+from mediapipe.solutions import drawing_utils as mp_draw
+from mediapipe.solutions import hands as mp_hands
 import numpy as np
 import threading
 from deepface import DeepFace
